20396996c3eeb791aa52f7ce0e082456f3c7e1d8
Paulo Gabriel <pgj1085@gmail.com>
2021-10-26 20:44:05 -0300

    Initial tcp listener

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,10 @@
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
+
+	for stream in listener.incoming() {
+		let _stream = stream.unwrap();
+		println!("Connection established!");
+	}
 }
