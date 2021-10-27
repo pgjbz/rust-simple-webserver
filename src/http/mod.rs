c4dfd306cdbad2b4446930e1be6f5b27a016c390
Paulo Gabriel <pgj1085@gmail.com>
2021-10-27 13:52:28 -0300

    Use struct to perform requests

--- src/http/mod.rs
+++ src/http/mod.rs
@@ -1,31 +1,27 @@
-
-
 use std::collections::HashMap;
 use std::fs;
 use std::net::TcpStream;
 use std::io::prelude::*;
-use std::sync::Mutex; // into scope to get access to certain traits 
+use std::sync::Mutex;
 
 const GET_INIT:  &'static [u8] = b"GET";
 
-pub type Routes = HashMap::<String, fn(TcpStream)>;
+pub type Routes = HashMap::<String, fn(&mut Request)>;
 
-fn root_exec<'r>(stream: TcpStream) {
-	let content = fs::read_to_string("view/hello.html").unwrap();
-	write_content(stream, &content, HttpStatus::Ok);
+pub struct Request<'a> {
+	stream: &'a mut TcpStream,
+	pub path: String,
+	pub method: HttpMethod
 }
 
-pub fn default_not_found(stream: TcpStream) {
-	let content = fs::read_to_string("view/404.html").unwrap();
-	write_content(stream, &content, HttpStatus::NotFound);
-}
-
-lazy_static! {
-    pub static ref ROUTES: Mutex<Routes> = {
-        let mut routes = Routes::new();
-		routes.insert(String::from("/"), root_exec);
-        Mutex::new(routes)
-    };    
+impl<'a> Request<'a> {
+	pub fn new(method: HttpMethod, path: String, stream: &'a mut TcpStream) -> Self {
+		Self {
+			stream,
+			path, 
+			method
+		}
+	}
 }
 
 pub enum HttpMethod {
@@ -48,6 +44,34 @@ impl HttpStatus {
 	}
 }
 
+pub fn get_request(stream: &mut TcpStream) -> Box<Request> {
+	let buffer = read_buffer(stream); 
+	let method = parse_http_method(&buffer);
+	let path = if let Ok(path) = extract_path(&buffer) {
+		path
+	} else {
+		"404".to_string()
+	};
+	Box::new(Request::new(method, path, stream))
+}
+
+fn root_exec(request: &mut Request) {
+	let content = fs::read_to_string("view/hello.html").unwrap();
+	write_content(request, &content, HttpStatus::Ok);
+}
+
+pub fn default_not_found(request: &mut Request) {
+	let content = fs::read_to_string("view/404.html").unwrap();
+	write_content(request, &content, HttpStatus::NotFound);
+}
+
+lazy_static! {
+    pub static ref ROUTES: Mutex<Routes> = {
+        let mut routes = Routes::new();
+		routes.insert(String::from("/"), root_exec);
+        Mutex::new(routes)
+    };    
+}
 
 pub fn parse_http_method(buffer: &[u8]) -> HttpMethod {
 	if buffer.starts_with(GET_INIT) {
@@ -57,7 +81,7 @@ pub fn parse_http_method(buffer: &[u8]) -> HttpMethod {
 	}
 }
 
-pub fn write_content(mut stream: TcpStream, content: &str, status: HttpStatus) {
+fn write_content(request: &mut Request, content: &str, status: HttpStatus) {
 								//HTTP-Version Status-Code Reason-Phrase CRLF
 	let response = format!("HTTP/1.1 {} {}\r\nContent-Length: {}\r\n\r\n{}", 
 		status as u32, 
@@ -65,17 +89,18 @@ pub fn write_content(mut stream: TcpStream, content: &str, status: HttpStatus) {
 		content.len(), 
 		content);
 
-	stream.write(response.as_bytes()).unwrap();
-	stream.flush().unwrap();
+	request.stream.write(response.as_bytes()).unwrap();
+	request.stream.flush().unwrap();
+	
 }	
 
-pub fn read_buffer(stream: &mut TcpStream) -> [u8; 1024] {
+fn read_buffer(stream: &mut TcpStream) -> [u8; 1024] {
 	let mut buffer = [0; 1024];
 	stream.read(&mut buffer).unwrap();
 	buffer
 }
 
-pub fn extract_path(buffer: &[u8]) -> Result<String, ()> {
+fn extract_path(buffer: &[u8]) -> Result<String, ()> {
 	let str = String::from_utf8_lossy(buffer);
 	let str_slipt = str.split_whitespace();
 	if let Some(val) = str_slipt.skip(1).next() {
--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,25 @@
-use std::net::{TcpListener, TcpStream};
+use std::{net::{TcpListener, TcpStream}};
 
-use rust_simple_webserver::http::{HttpMethod, ROUTES, default_not_found, extract_path, parse_http_method, read_buffer};
+use rust_simple_webserver::http::{HttpMethod, ROUTES, default_not_found, get_request};
 
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
 	for stream in listener.incoming() {
-		let stream = stream.unwrap();
+		let stream = &mut stream.unwrap();
 		handle_connection(stream);
 	}
 }
 
-fn handle_connection(mut stream: TcpStream) {
-	let buffer = read_buffer(&mut stream);
+fn handle_connection(stream: &mut TcpStream) {
 
-	if let HttpMethod::GET = parse_http_method(&buffer) {
-		let path = if let Ok(path) = extract_path(&buffer) {
-			path
+	let request = &mut get_request(stream);
+	if let HttpMethod::GET = request.method {
+		if let Some(ex) = ROUTES.lock().unwrap().get(&request.path) {
+			ex(request);
 		} else {
-			"/404".to_string()
-		};
-
-		if let Some(ex) = ROUTES.lock().unwrap().get(&path) {
-			ex(stream);
-		} else {
-			default_not_found(stream);
+			default_not_found(request);
 		}
 	}
 }
