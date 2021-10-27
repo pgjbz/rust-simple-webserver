c0ad450f9b9d0be01dd314285753e07f782ce0cf
Paulo Gabriel <pgj1085@gmail.com>
2021-10-26 21:35:44 -0300

    Reading request

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,18 @@
-use std::net::TcpListener;
+use std::io::prelude::*; // into scope to get access to certain traits 
+use std::net::{TcpListener, TcpStream};
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
 
 	for stream in listener.incoming() {
-		let _stream = stream.unwrap();
-		println!("Connection established!");
+		let stream = stream.unwrap();
+		handle_connection(stream);
 	}
+}
+
+fn handle_connection(mut stream: TcpStream) {
+	let mut buffer = [0; 1024];
+	stream.read(&mut buffer).unwrap();
+
+	println!("Request {}", String::from_utf8_lossy(&buffer[..]));
 }
