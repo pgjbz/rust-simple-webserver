b7147938797f5a65d70933d5c07ebcb59cfe6877
Paulo Gabriel <pgj1085@gmail.com>
2021-10-26 22:05:26 -0300

    Return a html to browser

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+use std::fs;
 use std::io::prelude::*; // into scope to get access to certain traits 
 use std::net::{TcpListener, TcpStream};
+
 
 fn main() {
     let listener = TcpListener::bind("127.0.0.1:8080").unwrap();
@@ -13,7 +15,12 @@ fn main() {
 fn handle_connection(mut stream: TcpStream) {
 	let mut buffer = [0; 1024];
 	stream.read(&mut buffer).unwrap();
-					 //HTTP-Version Status-Code Reason-Phrase CRLF
-	stream.write(b"HTTP/1.1 200 OK\r\n\r\n").unwrap();
+	let content = fs::read_to_string("view/hello.html").unwrap();
+					 			//HTTP-Version Status-Code Reason-Phrase CRLF
+	let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}",
+							content.len(),
+							content
+						);
+	stream.write(response.as_bytes()).unwrap();
 	stream.flush().unwrap();
 }
