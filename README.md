f34d393bb8902b7729801b4a8f3b8c25aa870b03
Paulo Gabriel <pgj1085@gmail.com>
2021-10-28 17:11:12 -0300

    Greacefull shutdown

--- README.md
+++ README.md
@@ -7,3 +7,12 @@ Source: https://doc.rust-lang.org/book/ch20-00-final-project-a-web-server.html
 To build this project are used
  - Cargo 1.56.0
  - Rust 1.56.0
+
+
+Ideas to implement from book:
+
+- Add more documentation to ThreadPool and its public methods.
+- Add tests of the library’s functionality.
+- Change calls to unwrap to more robust error handling.
+- Use ThreadPool to perform some task other than serving web requests.
+- Find a thread pool crate on crates.io and implement a similar web server using the crate instead. Then compare its API and robustness to the thread pool we implemented.
--- src/thread_pool/mod.rs
+++ src/thread_pool/mod.rs
@@ -2,7 +2,7 @@ use std::{sync::{Arc, Mutex, mpsc::{self}}, thread};
 
 pub struct ThreadPool{
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: mpsc::Sender<Message>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -36,22 +36,57 @@ impl ThreadPool {
         F: FnOnce() + Send + 'static    
     {
         let job = Box::new(f);
-        self.sender.send(job).unwrap();
+        self.sender.send(Message::NewJob(job)).unwrap();
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+
+        println!("Sending terminate message to all workers.");
+
+        for _ in &self.workers {
+            self.sender.send(Message::Terminate).unwrap();
+        }
+
+        println!("Shutdown all workers.");
+
+        for worker in &mut self.workers {
+            println!("Shutdown worker {}", worker.id);
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap();
+            }
+        }
     }
 }
 
 struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
+}
+
+enum Message {
+    NewJob(Job),
+    Terminate
 }
 
 impl Worker {
-    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Self {
+    fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Message>>>) -> Self {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap(); //while let (and if let and match) does not drop temporary values until the end of the associated block. 
-            println!("Worker {} got a job; executing.", id);
-            job();
+            let message = receiver.lock().unwrap().recv().unwrap(); //while let (and if let and match) does not drop temporary values until the end of the associated block. 
+            
+            match message {
+                Message::NewJob(job) => {
+                    println!("Worker {} got a job; executing.", id);
+                    job();
+                },
+                Message::Terminate => {
+                    println!("Worker {} was told to terminate", id);
+                    break;
+                }
+            }
+            
         });
-        Self { id: id, thread }
+        Self { id: id, thread: Some(thread) }
     }
 }
